550eea9d6b652c46530ea7b1c36f7c369e1c6dd3
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-03-25 09:03:33 -0300

    UpDate

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicialMapa = {
+    latitude: -10,
+    longitude: -55,
+    /* Definição do zoom do mapa.
+    Quanto menor, mais próximo o mapa fica.
+    Quanto mair, mais longe o mapa fica.*/
+    latitudeDelta: 40,
+    longitudeDelta: 40,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={estilos.container}>
+        <MapView
+          mapType="hybrid"
+          style={estilos.mapa}
+          userInterfaceStyle="dark" //somente ios
+          initialRegion={regiaoInicialMapa}
+          maxZoomLevel={15}
+          minZoomLevel={5}
+        />
+      </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mapa: { width: "100%", height: "100%" },
 });
